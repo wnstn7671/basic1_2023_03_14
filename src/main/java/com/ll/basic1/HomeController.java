dee673b7f12330bf1fe7bb0d5ede7cb08b366a56
wnstn7671 <wnstn7671@naver.com>
2023-03-14 13:00:07 +0900

    /home/plus

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller 의 의미
@@ -11,44 +12,53 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
 
-            private int num ;
+        private int num ;
 
-            public HomeController() {
-                num=-1;
-            }
+        public HomeController() {
+            num=-1;
+        }
 
-    // @GetMapping("/home/main") 의 의미
-            // 개발자가 스프링부트에게 말한다.
-            // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
-            @GetMapping("/home/main")
-            // @ResponseBody 의 의미
-            // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
-            @ResponseBody
-            public String showMain() {
-                return "안녕하세요.";
+// @GetMapping("/home/main") 의 의미
+        // 개발자가 스프링부트에게 말한다.
+        // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
+        @GetMapping("/home/main")
+        // @ResponseBody 의 의미
+        // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
+        @ResponseBody
+        public String showMain() {
+            return "안녕하세요.";
 
-    }
-            @GetMapping("/home/main2")
-            @ResponseBody
-            public String showMain2()
-            {
-                return" 반갑습니다.";
-            }
+}
+        @GetMapping("/home/main2")
+        @ResponseBody
+        public String showMain2()
+        {
+            return" 반갑습니다.";
+        }
 
-            @GetMapping("/home/main3")
-            @ResponseBody
-            public String showMain3()
-            {
-                return" 즐거웠습니다.";
-            }
+        @GetMapping("/home/main3")
+        @ResponseBody
+        public String showMain3()
+        {
+            return" 즐거웠습니다.";
+        }
 
-            @GetMapping("home/increase")
-            @ResponseBody
-            public int increase()
-            {
-                num++;
-                return num;
+        @GetMapping("home/increase")
+        @ResponseBody
+        public int showincrease()
+        {
+            num++;
+            return num;
 
-            }
+        }
+        @GetMapping("/home/plus")
+        @ResponseBody
+        // @RequestParam 의 의미
+        // 개발자가 스프링부트에게 말한다.
+        // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+        // @RequestParam 는 생략가능
+        public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+            return a + b;
+        }
 
 }
