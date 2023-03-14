eb220b2e316318f93fd5f34b6e705a100bcc78a3
wnstn7671 <wnstn7671@naver.com>
2023-03-14 14:46:55 +0900

    사람 추가 수정

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,64 +1,104 @@
 package com.ll.basic1;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    private int count;
+    private List<Person> people;
+
+    public HomeController() {
+        count = -1;
+        people = new ArrayList<>();
+    }
+
+    // @GetMapping("/home/main") 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
+    @GetMapping("/home/main")
+    // @ResponseBody 의 의미
+    // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+        count++;
+        return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // @RequestParam 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age) {
+        Person p = new Person(name, age);
+
+        System.out.println(p);
+
+        people.add(p);
+
+        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
+    }
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Person {
+    private static int lastId;
+    private final int id;
+    private final String name;
+    private final int age;
 
 
-        private int num ;
+    static {
+        lastId = 0;
+    }
 
-        public HomeController() {
-            num=-1;
-        }
-
-// @GetMapping("/home/main") 의 의미
-        // 개발자가 스프링부트에게 말한다.
-        // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
-        @GetMapping("/home/main")
-        // @ResponseBody 의 의미
-        // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
-        @ResponseBody
-        public String showMain() {
-            return "안녕하세요.";
-
-}
-        @GetMapping("/home/main2")
-        @ResponseBody
-        public String showMain2()
-        {
-            return" 반갑습니다.";
-        }
-
-        @GetMapping("/home/main3")
-        @ResponseBody
-        public String showMain3()
-        {
-            return" 즐거웠습니다.";
-        }
-
-        @GetMapping("home/increase")
-        @ResponseBody
-        public int showincrease()
-        {
-            num++;
-            return num;
-
-        }
-        @GetMapping("/home/plus")
-        @ResponseBody
-        // @RequestParam 의 의미
-        // 개발자가 스프링부트에게 말한다.
-        // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
-        // @RequestParam 는 생략가능
-        public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
-            return a + b;
-        }
-
+    Person(String name, int age) {
+        this(++lastId, name, age);
+    }
 }
