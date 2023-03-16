2ea51ec9026da9eeb3992a708dc0ab620840b409
wnstn7671 <wnstn7671@naver.com>
2023-03-16 13:02:31 +0900

    로그인을 세션으로 구현

--- src/main/java/com/ll/basic1/base/rq/Rq.java
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -3,11 +3,13 @@ package com.ll.basic1.base.rq;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Arrays;
+import java.util.Enumeration;
 
 @Component
 @RequestScope // 이 객체는 매 요청마다 생성된다.
@@ -64,5 +66,53 @@ public class Rq {
 
     public void setCookie(String name, String value) {
         resp.addCookie(new Cookie(name, value));
+    }
+
+    public void setSession(String name, long value) {
+        HttpSession session = req.getSession();
+        session.setAttribute(name, value);
+    }
+
+    public long getSessionAsLong(String name, long defaultValue) {
+        try {
+            long value = (long) req.getSession().getAttribute(name);
+            return value;
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    private String getSessionAsStr(String name, String defaultValue) {
+        try {
+            String value = (String) req.getSession().getAttribute(name);
+            if (value == null) return defaultValue;
+            return value;
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    public boolean removeSession(String name) {
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute(name) == null) return false;
+
+        session.removeAttribute(name);
+        return true;
+    }
+
+    // 디버깅용 함수
+    public String getSessionDebugContents() {
+        HttpSession session = req.getSession();
+        StringBuilder sb = new StringBuilder("Session content:\n");
+
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            Object attributeValue = session.getAttribute(attributeName);
+            sb.append(String.format("%s: %s\n", attributeName, attributeValue));
+        }
+
+        return sb.toString();
     }
 }
--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -31,7 +31,7 @@ public class MemberController {
 
         if (rsData.isSuccess()) {
             Member member = (Member) rsData.getData();
-            rq.setCookie("loginedMemberId", member.getId());
+            rq.setSession("loginedMemberId", member.getId());
         }
 
         return rsData;
@@ -40,7 +40,7 @@ public class MemberController {
     @GetMapping("/member/logout")
     @ResponseBody
     public RsData logout() {
-        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+        boolean cookieRemoved = rq.removeSession("loginedMemberId");
 
         if (cookieRemoved == false) {
             return RsData.of("S-2", "이미 로그아웃 상태입니다.");
@@ -52,7 +52,7 @@ public class MemberController {
     @GetMapping("/member/me")
     @ResponseBody
     public RsData showMe() {
-        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
+        long loginedMemberId = rq.getSessionAsLong("loginedMemberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
 
@@ -62,5 +62,12 @@ public class MemberController {
         Member member = memberService.findById(loginedMemberId);
 
         return RsData.of("S-1", "당신의 username(은)는 %s 입니다.".formatted(member.getUsername()));
+    }
+
+    // 디버깅용 함수
+    @GetMapping("/member/session")
+    @ResponseBody
+    public String showSession() {
+        return rq.getSessionDebugContents().replaceAll("\n", "<br>");
     }
 }
