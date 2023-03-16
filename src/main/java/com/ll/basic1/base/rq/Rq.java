4b1230925fe01d8484972cc9e6de8bb58b72f6e5
wnstn7671 <wnstn7671@naver.com>
2023-03-16 11:13:25 +0900

    32강까지 커밋

---
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -0,0 +1,68 @@
+package com.ll.basic1.base.rq;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Arrays;
+
+@Component
+@RequestScope // 이 객체는 매 요청마다 생성된다.
+@AllArgsConstructor
+public class Rq {
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+
+    public boolean removeCookie(String name) {
+        if (req.getCookies() != null) {
+            Cookie cookie = Arrays.stream(req.getCookies())
+                    .filter(c -> c.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
+
+            if (cookie != null) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String getCookie(String name, String defaultValue) {
+        if (req.getCookies() == null) return defaultValue;
+
+        return Arrays.stream(req.getCookies())
+                .filter(cookie -> cookie.getName().equals(name))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(defaultValue);
+    }
+
+    public long getCookieAsLong(String name, long defaultValue) {
+        String value = getCookie(name, null);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    public void setCookie(String name, long value) {
+        setCookie(name, value + "");
+    }
+
+    public void setCookie(String name, String value) {
+        resp.addCookie(new Cookie(name, value));
+    }
+}
---
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -0,0 +1,66 @@
+package com.ll.basic1.boundedContext.member.controller;
+
+import com.ll.basic1.base.rq.Rq;
+import com.ll.basic1.base.rsData.RsData;
+import com.ll.basic1.boundedContext.member.entity.Member;
+import com.ll.basic1.boundedContext.member.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @GetMapping("/member/login")
+    @ResponseBody
+    public RsData login(String username, String password) {
+        if (username == null || username.trim().length() == 0) {
+            return RsData.of("F-3", "username(을)를 입력해주세요.");
+        }
+
+        if (password == null || password.trim().length() == 0) {
+            return RsData.of("F-4", "password(을)를 입력해주세요.");
+        }
+
+        RsData rsData = memberService.tryLogin(username, password);
+
+        if (rsData.isSuccess()) {
+            Member member = (Member) rsData.getData();
+            rq.setCookie("loginedMemberId", member.getId());
+        }
+
+        return rsData;
+    }
+
+    @GetMapping("/member/logout")
+    @ResponseBody
+    public RsData logout() {
+        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+
+        if (cookieRemoved == false) {
+            return RsData.of("S-2", "이미 로그아웃 상태입니다.");
+        }
+
+        return RsData.of("S-1", "로그아웃 되었습니다.");
+    }
+
+    @GetMapping("/member/me")
+    @ResponseBody
+    public RsData showMe() {
+        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
+
+        boolean isLogined = loginedMemberId > 0;
+
+        if (isLogined == false)
+            return RsData.of("F-1", "로그인 후 이용해주세요.");
+
+        Member member = memberService.findById(loginedMemberId);
+
+        return RsData.of("S-1", "당신의 username(은)는 %s 입니다.".formatted(member.getUsername()));
+    }
+}
