0c28d4bf573816ec331d9c9baea7216de176f3a3
wnstn7671 <wnstn7671@naver.com>
2023-03-17 11:54:35 +0900

    ㄴJPA를 통해서 insert

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -3,17 +3,22 @@ package com.ll.basic1.boundedContext.article.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Builder
+@RequiredArgsConstructor
+
 public class Article {
-    @Id
+    @Id // PRIMARY KEY
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private long id;
-    private LocalDateTime createDate; //데이터 생성 날짜
+    private LocalDateTime createDate; // 데이터 생성 날짜
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
